c4e7456bdc7f8ec6db95e76f830c067d1ecf2b6c
calmisential <xinyu_std@163.com>
2020-05-25 21:40:04 +0800

    Update

---
+++ core/loss.py
@@ -0,0 +1,9 @@
+import torch
+
+
+class YoloLoss:
+    def __init__(self):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
--- core/procedure.py
+++ core/procedure.py
@@ -2,12 +2,15 @@ import torch
 
 from configuration import Config
 
+
 class PostProcessing:
     @staticmethod
     def training_procedure(yolo_outputs):
         generate_prediction = GeneratePrediction()
+        bboxes = []
         for i, feature in enumerate(yolo_outputs):
-            generate_prediction(feature=feature, feature_index=i)
+            bbox = generate_prediction(feature=feature, feature_index=i)
+            bboxes.append(bbox)
 
 
     def testing_procedure(self):
@@ -26,32 +29,34 @@ class GeneratePrediction:
         x = torch.arange(start=0, end=size[1], dtype=torch.float32)
         y = torch.arange(start=0, end=size[0], dtype=torch.float32)
         x, y = torch.meshgrid([x, y])
-        xy_grid = torch.stack(tensors=(x, y), dim=0)
+        xy_grid = torch.stack(tensors=(x, y), dim=-1)
+        xy_grid = torch.unsqueeze(xy_grid, dim=2)
         xy_grid = torch.unsqueeze(xy_grid, dim=0)
-        xy_grid = torch.unsqueeze(xy_grid, dim=0)
-        xy_grid = xy_grid.repeat(B, 3, 1, 1, 1)
+        xy_grid = xy_grid.repeat(B, 1, 1, 3, 1)
         return xy_grid
 
     def __call__(self, feature, feature_index, *args, **kwargs):
+        """
+        Generate predictions for one of the features output by yolo.
+        :param feature:
+        :param feature_index:
+        :param args:
+        :param kwargs:
+        :return: Tensor, size: (batch_size, feature_map_size, feature_map_size, 3, num_classes + 5)
+        """
+        feature = feature.permute(0, 2, 3, 1)
         shape = feature.size()
-        feature = torch.reshape(feature, (shape[0], 3, -1, shape[2], shape[3]))
-        # print(feature.size())
-        # torch.Size([2, 3, 25, 52, 52])
-        # torch.Size([2, 3, 25, 26, 26])
-        # torch.Size([2, 3, 25, 13, 13])
-        dx_dy, dw_dh, conf, prob = torch.split(feature, [2, 2, 1, self.num_classes], 2)
+        feature = torch.reshape(feature, (shape[0], shape[1], shape[2], 3, -1))
+        dx_dy, dw_dh, conf, prob = torch.split(feature, [2, 2, 1, self.num_classes], -1)
 
-        # print(dx_dy.shape, conf.shape)  # torch.Size([2, 3, 2, 52, 52]) torch.Size([2, 3, 1, 52, 52])
-        xy_grid = GeneratePrediction.__meshgrid(size=(shape[2], shape[3]), B=shape[0])
-        # print(xy_grid.size(), xy_grid.dtype)  # torch.Size([2, 3, 2, 52, 52]) torch.float32
+        xy_grid = GeneratePrediction.__meshgrid(size=shape[1:3], B=shape[0])
+
         pred_xy = self.strides[feature_index] * (torch.sigmoid(dx_dy) * self.scale[feature_index] - 0.5 * (self.scale[feature_index] - 1) + xy_grid)
-        print("pred_xy形状：", pred_xy.size())  # [2, 3, 2, 52, 52]
         pred_wh = torch.exp(dw_dh) * self.anchors[feature_index]
-
-        pred_xywh = torch.cat(tensors=(pred_xy, pred_wh), dim=1)
+        pred_xywh = torch.cat(tensors=(pred_xy, pred_wh), dim=-1)
         pred_conf = torch.sigmoid(conf)
         pred_prob = torch.sigmoid(prob)
-        pred_bbox = torch.cat(tensors=(pred_xywh, pred_conf, pred_prob), dim=1)
+        pred_bbox = torch.cat(tensors=(pred_xywh, pred_conf, pred_prob), dim=-1)
 
         return pred_bbox
 
