81c7efd32aa0fc2f093a0d89e0215dfcb1c9b8ce
calmisential <xinyu_std@163.com>
2020-06-02 18:43:01 +0800

    Resize the picture while maintaining the aspect ratio.

--- data/transform.py
+++ data/transform.py
@@ -6,16 +6,29 @@ class Rescale:
     def __init__(self, output_size):
         self.output_size = output_size
 
+    def __resize_with_pad(self, image, label):
+        h, w = image.shape[:2]
+        dst_h, dst_w = self.output_size
+        ratio = min(dst_h / h, dst_w / w)
+        new_h, new_w = h * ratio, w * ratio
+        image = cv2.resize(src=image, dsize=(new_w, new_h), interpolation=cv2.INTER_NEAREST)
+        top = (dst_h - new_h) // 2
+        bottom = dst_h - new_h - top
+        left = (dst_w - new_w) // 2
+        right = dst_w - new_w - left
+        resized_image = cv2.copyMakeBorder(src=image, top=top, bottom=bottom, left=left, right=right, borderType=cv2.BORDER_CONSTANT, value=128)
+
+        label[:, 0] = label[:, 0] * ratio + left
+        label[:, 1] = label[:, 1] * ratio + top
+        label[:, 2] = label[:, 2] * ratio + left
+        label[:, 3] = label[:, 3] * ratio + top
+
+        return resized_image, label
+
     def __call__(self, sample, *args, **kwargs):
         image, label = sample["image"], sample["label"]
-        h, w = image.shape[:2]
-        ratio = (self.output_size[0] / h, self.output_size[1] / w)
-        image = cv2.resize(src=image, dsize=(self.output_size[1], self.output_size[0]), interpolation=cv2.INTER_NEAREST)
         image = image / 255.0
-        label[:, 0] = label[:, 0] * ratio[1]
-        label[:, 1] = label[:, 1] * ratio[0]
-        label[:, 2] = label[:, 2] * ratio[1]
-        label[:, 3] = label[:, 3] * ratio[0]
+        image, label = self.__resize_with_pad(image, label)
         return {
             "image": image,
             "label": label
